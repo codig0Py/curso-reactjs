bdd0601ca0a24e6f6e367ded4cf8974a1c215b9c
codig0Py <bf3serratti@gmail.com>
2020-07-28 22:40:06 -0400

    Formulario de productos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 import ProductoList from './vistas/producto/ProductoList';
+import ProductoForm from './vistas/producto/ProductoForm';
 // import './App.css';
 
 class App extends Component {
  render() {
    return(
       <Container>
-          <ProductoList/>
+          {/* <ProductoList/> */}
+          <ProductoForm />
       </Container>
    )
  }
---
+++ src/vistas/producto/ProductoForm.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Row, Col, Form, Button } from 'react-bootstrap';
+
+
+export default class ProductoForm extends Component {
+    render() {
+        return (
+            <>
+                <Row>
+                    <Col><h1>Nuevo Producto</h1></Col>
+                </Row>
+                <Row>
+                    <Col xs={4} md={6}>
+                        <Form>
+                            <Form.Group>
+                                <Form.Label>Nombre del producto</Form.Label>
+                                <Form.Control type="text" placeholder="Inserte nombre del producto" />
+                                {/* <Form.Text className="text-muted">
+                                    Campo obligatorio
+                                </Form.Text> */}
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>Descripcion</Form.Label>
+                                <Form.Control type="text" placeholder="Inserte descripcion del producto" />
+                                {/* <Form.Text className="text-muted">
+                                    Campo obligatorio
+                                </Form.Text> */}
+                            </Form.Group>
+                        </Form>
+                    
+                    </Col>
+                    {/* <Col xs={2} md={4}>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                             when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+                    </Col> */}
+                </Row>
+                <Row>
+                    <Col md={6}>
+                        <Button variant="primary">Guardar</Button>{' '}
+                        <Button variant="danger">Volver</Button>
+                    </Col>
+                </Row>
+                
+            </>
+        )
+    }
+}
