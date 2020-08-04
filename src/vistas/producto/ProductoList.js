bdc38a480f0ac8628f0c8110c3786c8759e63394
msvzero <msvzero@gmail.com>
2020-08-03 20:45:43 -0400

    obtener productos

--- src/vistas/producto/ProductoList.js
+++ src/vistas/producto/ProductoList.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
 import { Row, Col, Button, Table } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
+import { db } from '../../config/firestore';
 
 class ProductoList extends Component {
+    state = {
+        listaProductos: []
+    }
+
+    componentDidMount() {
+        this.obtenerProductos();
+    }
     creaNuevoProducto = () => {
-        console.log('Nuevo producto');
-        console.log('Props del componente ProductoList: ', this.props)
+        // console.log('Nuevo producto');
+        // console.log('Props del componente ProductoList: ', this.props)
         this.props.history.push('/productos/nuevo')
+    }
 
+    obtenerProductos = () => {
+        let listaTemporal = [];
+        db.collection('productos').get()
+        .then((snap) => {
+            snap.forEach((documento) => {
+                // console.log(documento.data())
+                listaTemporal.push(documento.data());
+            })
+            this.setState({listaProductos: listaTemporal});
+            //NO HACER DE ESTA FORMA
+            // this.setState({
+            //     listaProductos:documento.data()
+            // })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
 
-        
+    renderListaProductos = () => {
+        return this.state.listaProductos.map((documento) => {
+            return (
+                // key es un identificador unico
+                <tr key={documento.producto}> 
+                    <td>{documento.producto}</td>
+                    <td>{documento.precioCompra}</td>
+                    <td>{documento.precioVenta}</td>
+                </tr>
+            )
+        })
     }
 
     render() {
+        const filasGeneradasdeLaTabla =  this.renderListaProductos();
         return (
             <div>
                <Row>
@@ -26,29 +64,18 @@ class ProductoList extends Component {
                                             <th>#</th>
                                             <th>Nombre</th>
                                             <th>Descripcion</th>
-                                            <th>Fecha de Carga</th>
+                                            {/* <th>Fecha de Carga</th> */}
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Bananas</td>
-                                            <td>De oro</td>
-                                            <td>19/02/2020</td>
-                                        </tr>
-                                        <tr>
-                                            <td>2</td>
-                                            <td>Bananas</td>
-                                            <td>De oro</td>
-                                            <td>19/02/2020</td>
-                                        </tr>
+                                    <tbody>{filasGeneradasdeLaTabla}
                                     </tbody>
-                                </Table>
+                        </Table>
                         </Col>
                </Row>
                <Row>
                    <Col>
-                        <Button variant="primary" onClick={this.creaNuevoProducto} >Agregar Producto</Button>
+                        <Button variant="primary" onClick={this.creaNuevoProducto} >Agregar Producto</Button> {' '}
+                        <Button variant="danger" onClick={()=> {console.log('ProductoList state: ', this.state)}} >Ver state</Button>
                    </Col>
                </Row>
             </div>
