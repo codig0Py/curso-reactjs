e3bcc8321ee4cf60631553e0c48e31482d4cf410
msvzero <msvzero@gmail.com>
2020-08-03 20:11:06 -0400

    alta de productos

--- src/vistas/producto/ProductoForm.js
+++ src/vistas/producto/ProductoForm.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
+import { db } from '../../config/firestore';
+
 
 
 class ProductoForm extends Component {
+    state={
+        producto: '',
+        precioCompra: 0,
+        precioVenta: 0,
+        calculox: 123
+    }
+
+    guardarProducto = () => {
+        let datosFinales = {
+            producto: this.state.producto,
+            precioCompra: this.state.precioCompra,
+            precioVenta: this.state.precioVenta,
+        }
+        console.log(this.state);
+        db.collection("productos").add(datosFinales)
+        .then(() => {
+            alert('Producto agregado con exito');
+        })
+        .catch((error) => {
+            console.log('ERROR: ', error)
+        })
+    }
+
+    setInputs = (evento) => {
+        this.setState({[evento.target.name]: evento.target.value})
+    }
     render() {
         return (
             <>
@@ -14,16 +42,24 @@ class ProductoForm extends Component {
                     <Col xs={4} md={6}>
                         <Form>
                             <Form.Group>
-                                <Form.Label>Nombre del producto</Form.Label>
-                                <Form.Control type="text" placeholder="Inserte nombre del producto" />
+                                <Form.Label>Producto</Form.Label>
+                                <Form.Control type="text" name="producto"  onChange={this.setInputs}/>
                                 {/* <Form.Text className="text-muted">
                                     Campo obligatorio
                                 </Form.Text> */}
                             </Form.Group>
 
                             <Form.Group>
-                                <Form.Label>Descripcion</Form.Label>
-                                <Form.Control type="text" placeholder="Inserte descripcion del producto" />
+                                <Form.Label>Precio compra</Form.Label>
+                                <Form.Control type="number" name="precioCompra"  onChange={this.setInputs}/>
+                                {/* <Form.Text className="text-muted">
+                                    Campo obligatorio
+                                </Form.Text> */}
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>Precio venta</Form.Label>
+                                <Form.Control type="number" name="precioVenta" onChange={this.setInputs}/>
                                 {/* <Form.Text className="text-muted">
                                     Campo obligatorio
                                 </Form.Text> */}
@@ -39,7 +75,7 @@ class ProductoForm extends Component {
                 </Row>
                 <Row>
                     <Col md={6}>
-                        <Button variant="primary">Guardar</Button>{' '}
+                        <Button variant="primary" onClick={this.guardarProducto}>Guardar</Button>{' '}
                         <Button variant="danger" onClick={() => {this.props.history.goBack()}}>Volver</Button>
                     </Col>
                 </Row>
