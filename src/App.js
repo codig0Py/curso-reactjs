8cf071ceab2bdedf7ec98659eaed8ffc07f6cc96
msvzero <msvzero@gmail.com>
2020-09-07 17:14:23 -0400

    Creacion de lista de usuarios

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MovimientoForm from './vistas/movimiento/MovimientoForm';
 import Informe from './vistas/informe/Informe';
 import Informe2 from './vistas/informe/Informe2';
 import Login from  './vistas/auth/Login';
+import UsuariosList from  './vistas/auth/UsuariosList';
 import PageNotFound from './vistas/error/PageNotFound';
 import ResetPassword from './vistas/auth/ResetPassword';
 import SignUp from './vistas/auth/SignUp';
@@ -89,6 +90,7 @@ render() {
              <PrivateRoute  path="/productos/editar/:id" component={ProductoForm} usuarioLogeado={this.state.usuarioLogeado} />
              <PrivateRoute  path="/productos/nuevo" component={ProductoForm} usuarioLogeado={this.state.usuarioLogeado} />
              <PrivateRoute  path="/movimientos" component={MovimientoForm} usuarioLogeado={this.state.usuarioLogeado} />
+             <PrivateRoute  path="/usuarios" component={UsuariosList} usuarioLogeado={this.state.usuarioLogeado} />
              <PublicRoute  exact path="/" component={Login} usuarioLogeado={this.state.usuarioLogeado} autenticacion={this.autenticacion}  />
              <PublicRoute  exact path="/resetpassword" component={ResetPassword} usuarioLogeado={this.state.usuarioLogeado}  />
              <PublicRoute  exact path="/signup" component={SignUp} usuarioLogeado={this.state.usuarioLogeado}  />
---
+++ src/vistas/auth/UsuariosList.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Row, Col, Button, Table } from 'react-bootstrap';
+import { confirmAlert } from 'react-confirm-alert';
+import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import { db } from '../../config/firestore';
+
+import { VscEdit, VscTrash } from "react-icons/vsc";
+
+class UsuariosList extends Component {
+    state = {
+        listaUsuarios: []
+    }
+
+    componentDidMount() {
+        this.obtenerUsuarios();
+    }
+   
+    obtenerUsuarios = () => {
+        let listaTemporal = [];
+        db.collection('usuarios').get()
+        .then((snap) => {
+            snap.forEach((documento) => {
+                listaTemporal.push({id: documento.id, ...documento.data()});
+            })
+            this.setState({listaUsuarios: listaTemporal});
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+
+    renderListaUsuarios = () => {
+        return this.state.listaUsuarios.map((documento) => {
+            return (
+                // key es un identificador unico
+                <tr key={documento.id}> 
+                    <td>{documento.email}</td>
+                    <td style={{textAlign:"center"}}>
+                        <Link to={`/productos/editar/${documento.id}`}><VscEdit/></Link>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
+    render() {
+        const filasGeneradasdeLaTabla =  this.renderListaUsuarios();
+        return (
+            <div>
+               <Row>
+                   <Col>Usuarios</Col>
+               </Row>
+               <br/>
+               <Row>
+                   <Col>
+                        <Table striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                            {/* <th>#</th> */}
+                                            <th>Email</th>
+                                            <th style={{textAlign:"center"}}>Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>{filasGeneradasdeLaTabla}
+                                    </tbody>
+                        </Table>
+                        </Col>
+               </Row>
+               
+            </div>
+
+        )
+                
+            
+        
+    }
+}
+
+
+export default UsuariosList;
