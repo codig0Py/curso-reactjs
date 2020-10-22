14857d2eafeaa2d2ce0e9cafb80c1197f2674193
msvzero <msvzero@gmail.com>
2020-10-22 16:37:47 -0300

    new package react-localization

--- src/vistas/Home.js
+++ src/vistas/Home.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import {Row, Col, Jumbotron, Button} from 'react-bootstrap';
 
+import LocalizedStrings from "react-localization";
+
+let strings = new LocalizedStrings({
+    es: {
+      bienvenido: "Bienvenido",
+      boton: "Aprenda mas"
+    },
+    en: {
+      bienvenido: "Welcome",
+      boton:"Learn more"
+    },
+    it: {
+        bienvenido: "Benvenuto",
+        boton: "Per saperne di più"
+    }
+  });
+
+strings.setLanguage('es');
+
 class Home extends Component {
     render() {
         return (
@@ -8,13 +27,13 @@ class Home extends Component {
                 <Row>
                     <Col md={12}>
                         <Jumbotron>
-                            <h1>Hello, world!</h1>
+                            <h1>{strings.bienvenido}</h1>
                             <p>
                                 This is a simple hero unit, a simple jumbotron-style component for calling
                                 extra attention to featured content or information.
                             </p>
                             <p>
-                                <Button variant="primary">Learn more</Button>
+                                <Button variant="primary">{strings.boton}</Button>
                             </p>
                         </Jumbotron>
                     </Col>
